466dc924555509e43a1c84458cb695d1e7047879
DayiEcheverria <dpecheverria48@misena.edu.co>
2017-01-18 17:51:33 -0500

    metodos ruby leccion

---
+++ metodos.rb
@@ -0,0 +1,14 @@
+def saludador (nombre)
+    return "hola #{nombre}"
+end
+saludador("carlos")
+
+def por_tres? (numero)
+    if numero % 3 == 0
+    return true
+else
+    return false
+end
+end
+por_tres?(28)
+gets()
